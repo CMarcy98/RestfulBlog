56e3c94615e25e132f4ea17881a6154b4b4c1001
Christian Marcy <christian.marcy@gmail.com>
2018-05-04 16:58:43 -0400

    Added Blog schema and model

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ express = require('express'),
 mongoose = require('mongoose'),
 app = express();
 
-// INITIALIZATION
+// SETTINGS
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// MONGOOSE/MODEL Config
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: {type: String, default: "https://cdn.pixabay.com/photo/2018/04/28/16/12/blueberry-3357568__480.jpg"},
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
 
 
 app.listen(8000, function() {
