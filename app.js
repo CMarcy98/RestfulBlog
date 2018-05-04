3078e45305c47e98d45a4184f6e535c63575b7a1
Christian Marcy <christian.marcy@gmail.com>
2018-05-04 17:30:55 -0400

    Blogs index route

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ express = require('express'),
 mongoose = require('mongoose'),
 app = express();
 
-// SETTINGS
-mongoose.connect('mongodb://localhost/restful_blog_app');
+// APP SETTINGS
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/restful_blog_app');
+app.use(express.static("public"));
 
 // MONGOOSE/MODEL Config
 var blogSchema = new mongoose.Schema({
@@ -17,6 +17,22 @@ var blogSchema = new mongoose.Schema({
 	created: {type: Date, default: Date.now}
 });
 var Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTful routes
+app.get("/", function(req, res) {
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(blogs);
+			res.render("index", {blogs: blogs});		}
+	});
+})
 
 
 app.listen(8000, function() {
