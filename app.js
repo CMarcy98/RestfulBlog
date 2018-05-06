b4ad72d60ee8a50e37621d8030ee50a3412038c1
Christian Marcy <christian.marcy@gmail.com>
2018-05-05 20:04:56 -0400

    Test commit from Intellij Idea

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var bodyParser = require('body-parser'),
+let bodyParser = require('body-parser'),
 express = require('express'),
 mongoose = require('mongoose'),
 app = express();
@@ -10,13 +10,13 @@ mongoose.connect('mongodb://localhost/restful_blog_app');
 app.use(express.static("public"));
 
 // MONGOOSE/MODEL Config
-var blogSchema = new mongoose.Schema({
+let blogSchema = new mongoose.Schema({
 	title: String,
 	image: {type: String, default: "https://cdn.pixabay.com/photo/2018/04/28/16/12/blueberry-3357568__480.jpg"},
 	body: String,
 	created: {type: Date, default: Date.now}
 });
-var Blog = mongoose.model("Blog", blogSchema);
+let Blog = mongoose.model("Blog", blogSchema);
 
 
 // RESTful routes
@@ -24,6 +24,7 @@ app.get("/", function(req, res) {
 	res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res) {
 	Blog.find({}, function(err, blogs) {
 		if(err) {
@@ -31,9 +32,27 @@ app.get("/blogs", function(req, res) {
 		} else {
 			res.render("index", {blogs: blogs});		}
 	});
-})
+});
 
+// NEW ROUTE
+app.get("/blogs/new", function(req,res) {
+	res.render("new");
+});
 
+// CREATE ROUTE
+app.post("/blogs", function(req,res) {
+	// Create blog
+	Blog.create(req.body.blog, function(err, newBlog) {
+		if(err) {
+			res.render("new");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+	// Redirect to blogs index
+});
+
+// Tells the app to listen on certain port
 app.listen(8000, function() {
 	console.log("Blog server is turned on!")
 });
